8470255488d3a3d53dfc04365806d273bc6d1fae
Ricardo Morin <morinricardo@gmail.com>
2025-01-27 15:31:12 -0800

    Replace addtomailgunlist with Netlify function

---
+++ functions/addtomailgunlist.mjs
@@ -0,0 +1,45 @@
+import Mailgun from 'mailgun.js'
+import FormData from 'form-data'
+
+import { text } from 'node:stream/consumers'
+import { Readable } from 'node:stream'
+
+export default async (request, context) => {
+  const apiKey = Netlify.env.get('MAILGUNAPIKEY')
+  const domain = Netlify.env.get('MAILGUNDOMAIN')
+  const mailinglist = Netlify.env.get('MAILINGLIST')
+  const notificationemail = Netlify.env.get('NOTIFICATIONEMAIL')
+
+
+  if (request.method === 'POST') {
+
+    const mailgun = new Mailgun(FormData)
+    const mg = mailgun.client({username: 'api', key: apiKey})
+
+    let bodyString = await text(Readable.from(request.body))
+
+    let member = {
+      subscribed: true,
+      address: new URLSearchParams(bodyString).get('email')
+    }
+
+    try {
+      let data = await mg.lists.members.createMember(mailinglist, member)
+
+      mg.messages.create(domain, {
+        from: 'Mailing list request<' + 'postmaster@' + domain + '>',
+        to: notificationemail,
+        subject: 'FYI: new address added to mailing list',
+        text: member.address + ' has been added to the mailing list ' + mailinglist
+      })
+
+      return new Response('Address added to mailing list')
+
+    } catch (e) {
+      return new Response(e.details, { status: e.status })
+    }
+
+  } else {
+    return new Response('Only POST method is allowed', { status: 405 })
+  }
+}
